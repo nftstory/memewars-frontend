e85c4a47e418d79f2f3911c73f8ce1e0c8af218b
Peter Ferguson <peterferguson95@gmail.com>
2023-10-03 19:54:15 +0100

    chore: add missing supabase client file

---
+++ src/lib/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABAS_ANON_KEY
+
+if (!SUPABASE_ANON_KEY || !SUPABASE_URL) {
+    throw new Error('Missing supabase env vars')
+}
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
